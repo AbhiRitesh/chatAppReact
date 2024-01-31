a06df705ff4fb04b0b4bd9daf76c5fe39766f4fc
Abhishek <absk08072002@gmail.com>
2024-02-01 02:20:36 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATkn3muP0VggS5vdjNttkt-On061BYNWc",
+  authDomain: "chatapp-41b79.firebaseapp.com",
+  projectId: "chatapp-41b79",
+  storageBucket: "chatapp-41b79.appspot.com",
+  messagingSenderId: "1041754196265",
+  appId: "1:1041754196265:web:1d9fff0c196ded64588f38"
+};
+
+export const app = initializeApp(firebaseConfig);
